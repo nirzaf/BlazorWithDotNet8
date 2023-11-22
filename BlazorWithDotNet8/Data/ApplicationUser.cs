20a6c63e4ebc51da442f9bab2cae070afadb85df
nirzaf <mfmfazrin1986@gmail.com>
2023-11-22 20:41:58 +0300

    Add blazor.webassembly.js to project
    A new JavaScript file, blazor.webassembly.js, has been added to the framework folder in the wwwroot directory. This enables the use of Blazor with .NET 8.0 in our WebAssembly application, improving application performance and enabling advanced features.

---
+++ BlazorWithDotNet8/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWithDotNet8.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
